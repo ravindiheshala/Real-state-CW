bb7a5e3c3e734f272760221279ae1b0169b90df0
Ravindi <ravindiheshala@gmail.com>
2026-01-01 06:01:56 -0800

    Create search form

--- src/components/searchForm.jsx
+++ src/components/searchForm.jsx
@@ -1,11 +1,19 @@
+import {useState} from 'react'
 import './Data.css'
 
 function Form() {
+
+  const[type,setType] =useState("")
 
   return (
     <>
       <form action="">
         <h1>Search properties</h1>
+        <select ID="type" value={type} onChange={(e)=> setType(e.target.value)} className='form-control'>
+          <option value="">Any</option>
+          <option value="">House</option>
+          <option value="">Flat</option>
+        </select>
         <div className=''></div>
       </form>
     </>
